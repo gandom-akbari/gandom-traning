bc54b49145864b6a6b7ef26c17b781fc173426b5
Gandom akbari <gandomkitty1385@gmail.com>
2026-08-01 13:34:13 -0700

    vid6

--- p1.py
+++ p1.py
@@ -13,3 +13,36 @@ y=3
 z=x+y
 print(z)
 
+x=["ali","gandom","mohamad","kitty","olaf"]
+print(x[2])
+print(type(x))
+print(x[2:5])
+print(x[0:5:2])
+print(x[:3])
+print(x[3:])
+print(x[1][-1])
+
+
+x=[1,2,3,5,"me"]
+y=[2,8]
+z=x+y
+print(z)
+x[4]=6
+print(x)
+
+x=[1,2,33,45,8]
+
+
+x[2:3]=["kitty"]
+print(x)
+
+x=["ali","elsa","moon","bus"]
+y=[1,2,3,4,5,6]
+z=x[1:3]+y[2:5]
+print(z)
+
+
+
+x=["ali","elsa","moon","bus"]
+for i in x:
+    print("i=",i)
