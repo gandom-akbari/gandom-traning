cf554ced6c93508719445a21a004683795b4f206
Gandom akbari <gandomkitty1385@gmail.com>
2026-08-04 06:46:54 -0700

    homework2

---
+++ Untitled-3.py
@@ -0,0 +1,17 @@
+x=["moon","sun",[-15,"stars"],"books","me"]
+print(x[2])
+print(x[2][0])
+
+z=[1,6,-5,12,-93,7]
+print("max=",max(z))
+print("min=",min(z))
+print("n=",len(z))
+
+y=[10,-5,9,23,12,1]
+y.insert(4,"reza")
+print(y)
+y.remove(9)
+y.remove(23)
+print(y)
+y[4]="ali"
+print(y)
