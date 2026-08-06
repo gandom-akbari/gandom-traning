b7c442af3e3c6d9b1804c5c12a591d881aada03f
Gandom akbari <gandomkitty1385@gmail.com>
2026-08-06 06:26:29 -0700

    homework

---
+++ homework.py
@@ -0,0 +1,35 @@
+print("whats the most beutifull thing in the world?")
+n=3
+f=0
+for i in range(n):
+      x=input("answer=")
+      if x=="aroura":
+            break
+      else:
+          if i<n-1:
+           print("try again loser")
+          f=f+1
+if f<n:
+     print("you won ")
+else:
+         print("you lose , next qestion")
+
+         print("what is the biggest star near earth??") 
+         n=3
+         fa=0
+         for i in range(n):
+          s=input("ansewr=")
+          if s=="sun":
+               break 
+          else:
+               fa=fa+1
+               print("try again stipid")
+
+
+
+
+
+if fa==n:
+    print("game overrr")   
+else:
+    print("you won,kiss")
