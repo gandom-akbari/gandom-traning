96c007cd95e0e461fe165a1d1166e8df808c8992
g.akbari <gandomkitty1385@gmail.com>
2026-09-16 10:55:23 +0330

    improvement of todo project by add txt file to store file

--- todo_project/password_for_todo_list.py
+++ todo_project/password_for_todo_list.py
@@ -1,5 +1,10 @@
 import re
 def password():
+    number=0
+    name=input ("hi welcome to todo ; whats your full name?")
+    age=input("how old are you?")
+    country=input("where are you from?")
+    print(name,age,"from",country,"we are so happy you choosed our application ; we hope its usefull for you :) ")
     print(" please make a password for your information secretory")
     while True:
      pas=input("Remember your pass should have 6-12 charecters including capital-small-nums ; " \
@@ -10,7 +15,14 @@ def password():
      n=len(pas)
      if n>=6 and n<=12 and num and smal_num and capital:
         print("Amazing;now you have a passkey")
-        break 
+        with open("todo.txt", "a") as file:
+            file.write(f"Name: {name}\n")
+            file.write(f"Age: {age}\n")
+            file.write(f"Country: {country}\n")
+            file.write(f"Password: {pas}\n")
+            number +=1
+
+        break
      else:
         print("try again honey ")
 
